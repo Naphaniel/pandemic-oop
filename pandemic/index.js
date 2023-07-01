c9fd785a7144a5fac1a96da7a5e2dcad07933d37
NStrickland <services+github@nstrickland.com>
2023-07-01 16:38:52 +0100

    remove js files

--- pandemic/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Game = void 0;
-var Game_1 = require("./Game");
-Object.defineProperty(exports, "Game", { enumerable: true, get: function () { return Game_1.Game; } });
